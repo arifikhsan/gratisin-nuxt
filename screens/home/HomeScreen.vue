17af9b42f83850fd58b5a8a2cd4bdbbb38eae72c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-06-06 19:20:41 +0700

    up

--- screens/home/HomeScreen.vue
+++ screens/home/HomeScreen.vue
@@ -136,16 +136,18 @@
           </div>
         </div>
 
+        <!-- hero -->
         <div
-          class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+          class="my-20 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
         >
           <div class="sm:text-center lg:text-left">
             <h2
               class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
             >
-              Cari barang atau jasa
-              <br class="xl:hidden" />
-              <span class="text-indigo-600">yang gratis!</span>
+              <span>
+                Cari barang atau jasa
+              </span>
+              <span class="text-indigo-600">yang gratis?</span>
             </h2>
             <p
               class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
@@ -170,12 +172,207 @@
                   href="#"
                   class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                 >
-                  Grup Discord
+                  Grup Telegram
                 </a>
               </div>
             </div>
           </div>
         </div>
+        <!-- end hero -->
+        <!-- start feature -->
+        <div class="py-12 bg-white">
+          <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="lg:text-center">
+              <p
+                class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase"
+              >
+                Konsep
+              </p>
+              <h3
+                class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+              >
+                Sebuah tempat untuk berbagi
+              </h3>
+              <p
+                class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto"
+              >
+                Tidak ada yang gratis. Pernyataan tersebut benar adanya, namun
+                masih ada kemungkinan kalau ada sesuatu yang benar benar gratis
+                di dunia ini.
+              </p>
+            </div>
+
+            <div class="mt-10">
+              <ul class="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+                <li>
+                  <div class="flex">
+                    <div class="flex-shrink-0">
+                      <div
+                        class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                      >
+                        <svg
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="ml-4">
+                      <h4 class="text-lg leading-6 font-medium text-gray-900">
+                        Akses Global
+                      </h4>
+                      <p class="mt-2 text-base leading-6 text-gray-500">
+                        Temui berbagai kemungkinan yang belum pernah anda
+                        ketahui adanya di dalam dunia maya.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+                <li class="mt-10 md:mt-0">
+                  <div class="flex">
+                    <div class="flex-shrink-0">
+                      <div
+                        class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                      >
+                        <svg
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="ml-4">
+                      <h4 class="text-lg leading-6 font-medium text-gray-900">
+                        Tidak ada biaya tersembunyi
+                      </h4>
+                      <p class="mt-2 text-base leading-6 text-gray-500">
+                        Tidak perlu khawatir adanya seseorang yang menyediakan
+                        sesuatu secara gratis, kemudian memukul dengan harga
+                        yang mahal.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+                <li class="mt-10 md:mt-0">
+                  <div class="flex">
+                    <div class="flex-shrink-0">
+                      <div
+                        class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                      >
+                        <svg
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M13 10V3L4 14h7v7l9-11h-7z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="ml-4">
+                      <h4 class="text-lg leading-6 font-medium text-gray-900">
+                        Tidak ada transaksi
+                      </h4>
+                      <p class="mt-2 text-base leading-6 text-gray-500">
+                        Tidak ada pembayaran untuk melakukan sesuatu secara
+                        gratis. Namun sebuah kebaikan pasti ada timbal baliknya.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+                <li class="mt-10 md:mt-0">
+                  <div class="flex">
+                    <div class="flex-shrink-0">
+                      <div
+                        class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
+                      >
+                        <svg
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div class="ml-4">
+                      <h4 class="text-lg leading-6 font-medium text-gray-900">
+                        Komunitas aktif
+                      </h4>
+                      <p class="mt-2 text-base leading-6 text-gray-500">
+                        Gabung grup telegram untuk mengetahui apa yang terjadi
+                        di belakang layar.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- end feature -->
+        <!-- start cta -->
+        <div class="bg-gray-50">
+          <div
+            class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between text-center"
+          >
+            <h2
+              class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+            >
+              Apakah Anda siap?
+              <br />
+              <span class="text-indigo-600"
+                >Saatnya untuk mencari sesuatu...</span
+              >
+            </h2>
+            <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0 justify-center">
+              <div class="inline-flex rounded-md shadow">
+                <nuxt-link
+                  to="/dashboard"
+                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                >
+                  Lihat Listing
+                </nuxt-link>
+              </div>
+              <div class="ml-3 inline-flex rounded-md shadow">
+                <a
+                  href="#"
+                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                >
+                  Grup Telegram
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end cta -->
       </div>
     </div>
   </div>
