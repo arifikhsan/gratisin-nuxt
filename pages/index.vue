3549fb4fe1d8f96fb8fc4df818161330154ededd
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-06-06 11:47:31 +0700

    up

--- pages/index.vue
+++ pages/index.vue
@@ -56,7 +56,7 @@
               <a
                 href="#"
                 class="ml-8 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out"
-                >Tentang Kami
+                >Tentang
               </a>
               <a
                 href="#"
@@ -119,22 +119,17 @@
                 <a
                   href="#"
                   class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                  >Product
+                  >Barang
                 </a>
                 <a
                   href="#"
                   class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                  >Features
+                  >Jasa
                 </a>
                 <a
                   href="#"
                   class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                  >Marketplace
-                </a>
-                <a
-                  href="#"
-                  class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                  >Company
+                  >Tentang
                 </a>
               </div>
               <div>
@@ -142,7 +137,7 @@
                   href="#"
                   class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out"
                 >
-                  Log in
+                  Login
                 </a>
               </div>
             </div>
