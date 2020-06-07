e7aa4ad2d413113dbeda26f855b277e824caf9a5
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-06-07 13:30:23 +0700

    up

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,9 @@
     <nuxt />
   </div>
 </template>
+
+<style>
+* {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
--- screens/home/HomeScreen.vue
+++ screens/home/HomeScreen.vue
@@ -187,12 +187,12 @@
               <p
                 class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase"
               >
-                Konsep
+                Sebuah Konsep
               </p>
               <h3
                 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
               >
-                Sebuah tempat untuk berbagi
+                Tempat untuk berbagi
               </h3>
               <p
                 class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto"
@@ -395,9 +395,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-* {
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-}
-</style>
